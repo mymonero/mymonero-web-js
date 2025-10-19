6b793e82f26a49035c02e66e48557937092c447b
Karl Buys <kbbuys@gmail.com>
2025-10-19 22:02:40 +0200

    build: build fixes

--- webpack.config.js
+++ webpack.config.js
@@ -53,7 +53,7 @@ module.exports = {
       stream: require.resolve('stream-browserify'),
       path: require.resolve('path-browserify'),
       process: require.resolve('process/browser'),
-      vm: require.resolve('vm-browserify')
+      vm: false 
     }
   }
 }
